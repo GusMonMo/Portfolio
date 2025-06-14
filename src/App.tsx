94d1677bfbf1164397850fd588726a5193ac5e02
GusMonMo <gustavo.mo.monteiro@gmail.com>
2025-06-14 09:51:41 -0400

    feat: background

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ function App() {
   return (
     <>
       <header>
-        Inicio Contato Projetos Currículo
+        
       </header>
       <div className="capa">
         <div className="background"/>
@@ -28,6 +28,25 @@ function App() {
            <div className="barra nav"/>
         </nav>
       </div>
+      <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', }}>
+        <div className="barra central"/>
+      </div>
+      <section>
+        <h2>QUEM SOU EU ?</h2>
+        <div className="barra section"/> <br />
+        <p>
+          Sou um acadêmico de engenharia de software e desenvolvedor front-end ambicionado em desenvolvimento de sistemas coorporativos e utilitários.
+        </p> <br />
+        <p>
+          Com dois anos de experiência usando TypeScript, ReactJs, NextJS, CSS, SASS e alguns frameworks desses, concluí alguns projetos pessoais que consolidaram muito do meu aprendizado e me capacitou a fazer a diferença em qualquer demanda.
+        </p> <br />
+        <p>
+          Atualmente estudo, além das tecnologias acima, NodeJS, Express e MySQL para ir além na minha stack.
+        </p>
+      </section>
+      <main>
+
+      </main>
     </>
   )
 }
