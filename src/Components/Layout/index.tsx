add25b0fc0013017946d010e6240e382ee0266c9
Gustavo <gusmonmodev@gmail.com>
2025-07-01 17:21:41 -0400

    feat: header nav

--- src/Components/Layout/index.tsx
+++ src/Components/Layout/index.tsx
@@ -1,4 +1,5 @@
 import ThemeButton from "@/Components/ThemeButton/index"
+import { Link } from "react-router-dom"
 import styles from './styles.module.scss'
 import { Outlet } from "react-router-dom"
 
@@ -7,6 +8,10 @@ export default function Layout(){
     return(
         <>
         <header className={styles.header}>
+            <nav>
+                <Link to='/'><h6>Home</h6></Link>
+                <Link to='/curriculo'><h6>Meu Currículo</h6></Link>
+            </nav>
             <ThemeButton/>
         </header>
         <Outlet/>
